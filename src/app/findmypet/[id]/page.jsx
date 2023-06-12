457feb10977da23b06379bb6dc5b43b609102633
CallJoshDrew <114333920+CallJoshDrew@users.noreply.github.com>
2023-06-12 18:34:32 +0800

    First Commit with Material Ui

---
+++ src/app/findmypet/[id]/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FindMyPet() {
+  return (
+    <div>Help!, My pet was lost!</div>
+  )
+}
---
+++ src/app/mypet/[id]/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MyPet() {
+  return (
+    <div>My Pet</div>
+  )
+}
