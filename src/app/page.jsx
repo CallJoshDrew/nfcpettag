6e8d949d0865b37ea6e130d1e8e0987c8021c4c1
CallJoshDrew <114333920+CallJoshDrew@users.noreply.github.com>
2023-06-29 17:27:59 +0800

    Designing Home Page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,16 +3,17 @@
 import {
   Box,
   Button,
-  Card,
-  CardContent,
-  CardMedia,
   CardActionArea,
-  CardActions,  
+  CardActions,
   Grid,
   Paper,
   Stack,
+  CardMedia,
   Typography,
 } from "@mui/material";
+import Card from "@mui/joy/Card";
+import CardCover from "@mui/joy/CardCover";
+import CardContent from "@mui/joy/CardContent";
 import Footer from "./components/FooterBar";
 import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
 import PetsIcon from "@mui/icons-material/Pets";
@@ -24,116 +25,82 @@ export default function Home() {
   const page = "./";
   const router = useRouter();
   return (
-    <Box
-      // height="100vh"
-      alignItems="center"
-      justifyContent="flex-start"
-      direction="column"
-      margin="90px 0 90px"
-    >
-      <Navbar />
-      <Typography variant="h6" fontWeight="bold" margin="20px 10px" padding="0 10px">
-        Dashboard
-      </Typography>
-      <Box
-        display="flex"
-        justifyContent="flex-start"
-        alignItems="center"
-        alignContent="center"
-        margin="10px"
-        padding="0 20px"
-      >
-        <PetsIcon />
-        <Typography marginLeft="5px">My Pet</Typography>
-      </Box>
-      <Box
-        display="flex"
-        justifyContent="flex-start"
-        alignItems="center"
-        alignContent="center"
-        margin="10px"
-        padding="0 10px"
-      >
-        <Card sx={{ display: "flex", padding: "10px" }}>
-          <CardMedia
-            component="img"
-            sx={{ width: 120, borderRadius: "4px" }}
-            image="/cat.jpg"
-            alt="chloe"
-          />
-          <Grid container justifyContent="flex-start" padding="0 15px">
-            <Grid item xs={12}>
-              <Typography variant="h7">Chloe</Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography variant="h7" color="text.secondary">
-                Age: 2 Years Old
-              </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography variant="h7" color="text.secondary">
-                Gender: Female
-              </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography variant="h7" color="text.secondary">
-                Pet Type: Dog
-              </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography variant="h7" color="text.secondary">
-                Breed: Shepherd
-              </Typography>
-            </Grid>
-            <Button
-              variant="contained"
-              color="success"
-              onClick={() => { setTimeout(() => router.push(`//mypets/chloe`), 1000); }}
-              size="small"
-              startIcon={<EditIcon />}
-              sx={{ padding: "8px 15px" }}
-            >
-              Edit
-            </Button>
-          </Grid>
-        </Card>
-      </Box>
-      <Link href="/addmorepets" style={{ textDecoration: "none" }}>
-        <Typography
-          sx={{ color: "red", fontWeight: "bold", paddingLeft: "30px" }}
-        >
-          Add More Pets
+    <Box>
+      <Box backgroundColor="black" align="center" padding="10px 0px">
+        <Typography variant="h7" fontWeight="bold" color="white">
+          "Help! I'm lost?"
         </Typography>
-      </Link>
-      <Box
-        display="flex"
-        direction="row"
-        justifyContent="flex-start"
-        alignItems="center"
-        alignContent="center"
-        margin="30px 10px 10px"
-        padding="0 10px"
-      >
-        <Card sx={{ display: "flex", padding: "10px" }}>
-          <Grid container justifyContent="flex-start" padding="0 15px">
-            <Typography>
-              Alert our communities when you loss your beloved pet. Just press
-              the "Help" Button to share your Pet Info with our communities.
-            </Typography>
-            <Button
-              variant="contained"
-              color="error"
-              href="./findmypet/chloe"
-              size="small"
-              startIcon={<EditIcon />}
-              sx={{ padding: "8px 15px", marginTop: "10px" }}
+      </Box>
+      <CardMedia component="img" height="240" image="/cat.jpg" alt="Dog" />
+      <Box backgroundColor="white" align="center" padding="10px 0px">
+        <Typography variant="h7" fontWeight="bold" color="#0E4B17">
+          No Worries! Just tap and get it home!
+        </Typography>
+      </Box>
+      <Box backgroundColor="#0E4B17" align="center" padding="30px 25px">
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          spacing={1.5}
+        >
+          <Grid item xs={12}>
+            <Box
+              component="ul"
+              sx={{ display: "flex", gap: 2, flexWrap: "wrap", p: 0, m: 0 }}
             >
-              Help
+              <Card component="li" sx={{ minWidth: 300, flexGrow: 1 }}>
+                <CardCover>
+                  <img
+                    src="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800"
+                    srcSet="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800&dpr=2 2x"
+                    loading="lazy"
+                    alt="NFC Chip"
+                  />
+                </CardCover>
+                <CardContent>
+                  <Typography
+                    level="h6"
+                    fontWeight="lg"
+                    color="#fff"
+                    mt={{ xs: 12, sm: 18 }}
+                  >
+                    NFC Chip Inside
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Box>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="body1" color="white">
+              Currently the best tags on the market with the latest technology
+              of NFC Chip. Our pet ID can locate your address without vet visits
+              for microchiop. With reliability and efficiency, your pet will be
+              home safe in no time!
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" color="success" fontWeight="bold">
+              Discover Our Smart Tags!
             </Button>
           </Grid>
-        </Card>
+        </Grid>
       </Box>
-      <Footer page={page} sx={{ zIndex: 100 }} />
+      <Card >
+        <CardContent sx={{ margin:"auto", width: "100%" }}>
+        <CardMedia
+          component="iframe"
+          height="200px"
+          autoPlay
+          controls
+          image="https://www.youtube.com/embed/3VfxVRQlPP4"
+        /></CardContent>
+        <CardContent>
+          <Typography align="center" variant="body2" color="textSecondary" component="p">
+            How to Use?
+          </Typography>
+        </CardContent>
+      </Card>
     </Box>
   );
 }
